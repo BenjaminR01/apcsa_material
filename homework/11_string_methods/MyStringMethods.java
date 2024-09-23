9ee6b84792180649b8c43b580ed8e7144817d1f0
BenjaminR01 <benjaminr156@nycstudents.net>
2024-09-23 19:43:17 -0400

    Update MyStringMethods.java

--- homework/11_string_methods/MyStringMethods.java
+++ homework/11_string_methods/MyStringMethods.java
@@ -1,19 +1,52 @@
 public class MyStringMethods{
 
-  // Explain how IndexOf method works in a few sentences
+  // IndexOf returns the index (position) of the first occurence of the characters it is searching for. It returns an int value.
   // Then implement your own myIndexOf
   public static int myIndexOf(String myString, String targetString){
-    return 0;
-  }                             
+    int index = -1;  
+    for (int i = 0; i < myString.length(); i++){
+      if (myString.substring(i, i + targetString.length()).equals(targetString)){
+        index = i;  
+        i = myString.length();
+      }
+      else{
+          index = -1;
+      }
+    }
+    return index;
+  }                          
 
-  // Explain how CompareTo method works in a few sentences/
+  // CompareTo compares two strings based off of the ASCII values of each character in the strings. If they are equal, 0 is returned. If the second string has more characters than the first, then the amount of extra characters will be returned.
   // Then implement your own myCompareTo
   public static int myCompareTo(String myString, String otherString){
-    return 0;
+    int result = 0;
+    if (myString.length() == otherString.length()){
+      for (int i = 0; i < myString.length(); i++){
+        if (myString.substring(i, i+1).equals(otherString.substring(i, i+1))){
+          result = 0;
+        }
+        else{
+          result = (int) (myString.charAt(i) - otherString.charAt(i));
+        }
+      }
+    }
+    else{
+      result = myString.length() - otherString.length();
+    }
+    return result;
   }
 
-  // Explain how parseInt method works in a few sentences/
+  // parseInt turns a String into a primitive int.
   // Then implement your own myParseInt()
  public static int myparseInt(String myString){
-
+   int digits = 0;
+   for (int i = 0; i < myString.length(); i++){
+     if (Character.isDigit(myString.substring(i, i+1)) == true){
+       digits += 1;
+     }
+   }
+   if (digits = myString.length()){
+     int newInt = (int) myString;
+   }
+   return newInt
  }
